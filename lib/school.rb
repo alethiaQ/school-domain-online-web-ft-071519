e5f6c4e357305596472463b48f78e4dfce1f0644
Alethia Quintero <alethiaq22@gmail.com>
2019-07-22 23:05:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,6 +11,10 @@ class School
   end
   def add_student(name, grade)
     @roster[grade] = [name] unless @roster.include?(grade)
+
+    if @roster.include?(grade)
+      @roster[grade] << name
+    
   end
 
 end
