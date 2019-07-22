729a5d2b6f23741ce9a0987a468d4a9d03e0cd54
Alethia Quintero <alethiaq22@gmail.com>
2019-07-22 23:09:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,21 +3,20 @@ class School
   attr_reader :roster
 
 
-
   def initialize(name)
     @name = name
     @roster = {}
-
   end
+
   def add_student(name, grade)
-
-
     if @roster.include?(grade)
       @roster[grade] << name
     else
       @roster[grade] = [name]
     end
-
   end
+
+  def grade=(num)
+    return @roster[num]
 
 end
